3254eb0c1b89c0819add6b0e40a28e97f50721b1
liucjray <liucjray@gmail.com>
2018-07-18 22:25:29 +0800

    fix class name error

--- app/Console/Commands/cmdDatabaseRebuildVoyager.php
+++ app/Console/Commands/cmdDatabaseRebuildVoyager.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class cmdDatabaseRebuildTesting extends Command
+class cmdDatabaseRebuildVoyager extends Command
 {
     /**
      * The name and signature of the console command.
